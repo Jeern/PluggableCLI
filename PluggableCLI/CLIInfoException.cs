1af6b43061efdd5d4c10f539d7229be10977d60c
Jesper Niedermann <jn@uvdata.dk>
2018-02-19 23:31:37 +0100

    Some validation added

---
+++ PluggableCLI/CLIInfoException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PluggableCLI
+{
+    [Serializable]
+    public class CLIInfoException : Exception
+    {
+        public CLIInfoException() { }
+        public CLIInfoException(string message) : base(message) { }
+        public CLIInfoException(string message, Exception inner) : base(message, inner) { }
+        protected CLIInfoException(SerializationInfo info,StreamingContext context) : base(info, context) { }
+    }
+}
--- PluggableCLI/CLIProviders.cs
+++ PluggableCLI/CLIProviders.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace PluggableCLI
@@ -7,12 +10,109 @@ namespace PluggableCLI
     {
         public static void Run(string[] args, Assembly executingAssembly = null)
         {
-            var assemblyName = (executingAssembly ?? Assembly.GetExecutingAssembly()).GetName().Name;
+            try
+            {
+                var assemblyName = (executingAssembly ?? Assembly.GetExecutingAssembly()).GetName().Name;
 
-            var loader = new ProviderLoader();
-            var providers = loader.LoadAllProviders();
+                var loader = new ProviderLoader();
+                var providers = loader.LoadAllProviders().ToList();
+                ValidateProviders(providers);
 
-            Console.WriteLine(assemblyName);
+                Console.WriteLine(assemblyName);
+
+            }
+            catch (CLIInfoException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static void ValidateProviders(List<ICLIProvider> providers)
+        {
+            ValidateUniqueNames(providers);
+            ValidateUniqueParameterNames(providers);
+            ValidateUniqueAppSettingNames(providers);
+            ValidateConnectionStringNames(providers);
+        }
+
+        private static void ValidateConnectionStringNames(List<ICLIProvider> providers)
+        {
+            foreach (var provider in providers)
+            {
+                ValidateConnectionStringNames(provider);
+            }
+        }
+
+        private static void ValidateConnectionStringNames(ICLIProvider provider)
+        {
+            if (provider.ConnectionStrings == null || provider.ConnectionStrings.Count == 0)
+                return;
+
+            ValidateUniqueNess(provider.ConnectionStrings.Select(p => p.Name.ToLowerInvariant()),
+                $"A connectionString in a  provider has to have a unique name. This is not the case with the connectionString {0} in {provider.Name}");
+        }
+        private static void ValidateUniqueAppSettingNames(List<ICLIProvider> providers)
+        {
+            foreach (var provider in providers)
+            {
+                ValidateUniqueAppSettingNames(provider);
+            }
+        }
+
+        private static void ValidateUniqueAppSettingNames(ICLIProvider provider)
+        {
+            if (provider.AppSettings == null || provider.AppSettings.Count == 0)
+                return;
+
+            ValidateUniqueNess(provider.AppSettings.Select(p => p.Name.ToLowerInvariant()),
+                $"An appSettings in a  provider has to have a unique name. This is not the case with the appSetting {0} in {provider.Name}");
+        }
+
+        private static void ValidateUniqueParameterNames(List<ICLIProvider> providers)
+        {
+            foreach (var provider in providers)
+            {
+                ValidateUniqueParameterNames(provider);
+            }
+        }
+
+        private static void ValidateUniqueParameterNames(ICLIProvider provider)
+        {
+            if (provider.Parameters == null || provider.Parameters.Count == 0)
+                return;
+
+            ValidateUniqueNess(provider.Parameters.Select(p => p.ShortName.ToLowerInvariant()),
+                $"A parameter in a  provider has to have a unique name. This is not the case with the parameter {0} in {provider.Name}");
+
+            ValidateUniqueNess(provider.Parameters.Select(p => p.LongName.ToLowerInvariant()),
+                $"A parameter in a  provider has to have a unique name. This is not the case with the parameter {0} in {provider.Name}");
+        }
+
+        private static void ValidateUniqueNess(IEnumerable<string> names, string errorMessageFormatTemplate)
+        {
+            //I Could just use a Distinct() and compare the count to the original count to find if there is a name
+            //that is not unique. But then I would not know which name it is. Therefore I use this hashSet technique
+            var hashSet = new HashSet<string>();
+            foreach (var name in names)
+            {
+                if (hashSet.Contains(name))
+                    throw new CLIInfoException(string.Format(errorMessageFormatTemplate, name));
+                hashSet.Add(name);
+            }
+        }
+
+        private static void ValidateUniqueNames(List<ICLIProvider> providers)
+        {
+            if(providers == null || providers.Count == 0)
+                throw new CLIInfoException("No providers found");
+
+            ValidateUniqueNess(providers.Select(p => p.Name.ToLowerInvariant()),
+                "A CLI provider has to have a unique name. This is not the case with {0}");
         }
     }
 }
