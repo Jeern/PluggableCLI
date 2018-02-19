742b78e865471555ab9f46401564134b8b4246e8
Jesper Niedermann <jn@uvdata.dk>
2018-02-19 16:56:35 +0100

    First commit

---
+++ PluggableCLI.TestConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PluggableCLI.TestConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PluggableCLI")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("153cce40-0d23-41d9-85d7-f664bed7907e")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ PluggableCLI/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PluggableCLI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PluggableCLI")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("cfa0fda8-4af4-4563-ac15-3a94a92f249c")]
+[assembly: AssemblyVersion("1.0.0.0")]
